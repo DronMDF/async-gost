70d4a4f7048981c8685bec1d687f6685054e1fbe
Andrey Valyaev <dron.valyaev@gmail.com>
2013-09-13 23:29:08 +0400

    Integrate boost::unit_test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#define BOOST_TEST_NO_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
 
 using namespace std;
 
-int main()
+BOOST_AUTO_TEST_CASE(emptyTest)
 {
+}
+
+int main(int argc, char **argv)
+{
+	if (boost::unit_test::unit_test_main([](){ return true; }, argc, argv) != 0) {
+		return -1;
+	}
+
 	cout << "Hello World!" << endl;
 	return 0;
 }
