ff53bdc5c8615284bea1448b2de6a3fb857705c3
Andrey Valyaev <dron.valyaev@gmail.com>
2013-09-20 23:41:02 +0400

    Функция гаммирования через движок

--- async-gost.cpp
+++ async-gost.cpp
@@ -1,14 +1,37 @@
 
 #include "async-gost.hpp"
+#include <cstring>
+#include "GostGenericEngine.h"
 
 using namespace std;
 
-future<ContextReply> async_cfb_encrypt(const vector<uint8_t> &data, const vector<uint8_t> &/*key*/,
-	const vector<uint8_t> &/*iv*/)
+ vector<uint8_t> gost_encrypt_cfb(const vector<uint8_t> &data, const vector<uint8_t> &key, const vector<uint8_t> &iv)
 {
-	return async([&]{
+	GostGenericEngine engine;
+	memcpy(&engine.key[0], &key[0], 32);
+	memcpy(&engine.iv[0], &iv[0], 8);
+
+	const size_t size = data.size();
+	vector<uint8_t> out(size);
+
+	uint32_t i[2];
+	for (size_t index = 0; index < size; index += 8) {
+		engine.encrypt();
+		memcpy(&i[0], &data[index], min(size - index, 8U));
+		engine.iv[0] ^= i[0];
+		engine.iv[1] ^= i[1];
+		memcpy(&out[index], &i[0], min(size - index, 8U));
+	}
+
+	return out;
+}
+
+future<ContextReply> async_cfb_encrypt(const vector<uint8_t> &data, const vector<uint8_t> &key,
+	const vector<uint8_t> &iv)
+{
+	return async([data, key, iv]{
 		ContextReply reply;
-		reply.data.assign(data.begin(), data.end());
+		reply.data = gost_encrypt_cfb(data, key, iv);
 		return reply;
 	});
 }
--- async-gost.pro
+++ async-gost.pro
@@ -3,6 +3,7 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS_RELEASE += -march=native -fomit-frame-pointer
 LIBS += -lboost_unit_test_framework -lboost_thread -ltbb
 
 SOURCES += main.cpp \
