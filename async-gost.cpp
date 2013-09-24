71f36d6279ab533f7274308926e6336e6537b695
Andrey Valyaev <dron.valyaev@gmail.com>
2013-09-24 21:04:43 +0400

    Чистка от старых алгоритмов

--- async-gost.cpp
+++ async-gost.cpp
@@ -8,28 +8,6 @@
 
 using namespace std;
 
-vector<uint8_t> gost_encrypt_cfb(const vector<uint8_t> &data, const vector<uint8_t> &key,
-				  const vector<uint8_t> &iv)
-{
-	GostGenericEngine engine;
-	memcpy(&engine.key[0], &key[0], 32);
-	memcpy(&engine.iv[0], &iv[0], 8);
-
-	const size_t size = data.size();
-	vector<uint8_t> out(size);
-
-	uint32_t i[2];
-	for (size_t index = 0; index < size; index += 8) {
-		engine.encrypt();
-		memcpy(&i[0], &data[index], min(size - index, size_t(8)));
-		engine.iv[0] ^= i[0];
-		engine.iv[1] ^= i[1];
-		memcpy(&out[index], &engine.iv[0], min(size - index, size_t(8)));
-	}
-
-	return out;
-}
-
 vector<uint8_t> gost_imit(const vector<uint8_t> &data, const vector<uint8_t> &key)
 {
 	GostGenericEngine engine;
@@ -48,29 +26,6 @@ vector<uint8_t> gost_imit(const vector<uint8_t> &data, const vector<uint8_t> &ke
 	vector<uint8_t> out(8);
 	memcpy(&out[0], &engine.iv[0], 8);
 	return out;
-}
-
-BOOST_AUTO_TEST_CASE(testGostShouldEncrypt)
-{
-	// Given
-	const vector<uint8_t> text = {
-		0xCC, 0xCC, 0xCC, 0xCC, 0x33, 0x33, 0x33, 0x33,
-		0x33, 0x33, 0x33, 0x33, 0xCC, 0xCC, 0xCC, 0xCC 	};
-	const vector<uint8_t> key = {
-		0x04, 0x75, 0xF6, 0xE0, 0x50, 0x38, 0xFB, 0xFA,
-		0xD2, 0xC7, 0xC3, 0x90, 0xED, 0xB3, 0xCA, 0x3D,
-		0x15, 0x47, 0x12, 0x42, 0x91, 0xAE, 0x1E, 0x8A,
-		0x2F, 0x79, 0xCD, 0x9E, 0xD2, 0xBC, 0xEF, 0xBD };
-	const vector<uint8_t> iv = {
-		0x2A, 0x80, 0xA7, 0xC3, 0xFF, 0xA8, 0xE3, 0x47, };
-	// When
-	const auto cypher = gost_encrypt_cfb(text, key, iv);
-	// Then
-	const vector<uint8_t> expected = {
-		0x3B, 0x4C, 0x22, 0x43, 0x10, 0xBD, 0x64, 0x4B,
-		0x99, 0xEB, 0x48, 0x55, 0xD9, 0x6C, 0xAA, 0x0F };
-	BOOST_REQUIRE_EQUAL_COLLECTIONS(cypher.begin(), cypher.end(),
-					expected.begin(), expected.end());
 }
 
 BOOST_AUTO_TEST_CASE(testGostShouldImit)
@@ -123,12 +78,6 @@ BOOST_AUTO_TEST_CASE(testGostShouldImit)
 }
 
 // Это основной интерфейс библиотеки
-struct ContextRequest {
-	promise<ContextReply> result;
-	vector<uint8_t> data;
-	vector<uint8_t> key;
-	vector<uint8_t> iv;
-};
 
 static vector<thread> crypto_threads;
 static tbb::concurrent_bounded_queue<shared_ptr<CryptoRequest>> crypto_encrypt_tasks;
